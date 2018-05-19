cc4554103546b29f13aa24b287335f6badad4722
Mat Ryer <thatmatryer@gmail.com>
2018-05-19 19:04:31 +0100

    ideas

--- generator/render_test.go
+++ generator/render_test.go
@@ -13,7 +13,7 @@ func TestRender(t *testing.T) {
 	is := is.New(t)
 	def, err := Parse("testdata/rpc/example")
 	is.NoErr(err)
-	b, err := ioutil.ReadFile("testdata/templates/go-server.plush.go")
+	b, err := ioutil.ReadFile("testdata/templates/go-json-server.plush.go")
 	is.NoErr(err)
 	var buf bytes.Buffer
 	err = Render(&buf, string(b), def)
