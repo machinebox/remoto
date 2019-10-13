99a4c9b6c341cdcfb7cab74d8dae25c0a1c18ded
matryer <thatmatryer@gmail.com>
2019-10-13 13:49:43 +0100

    added gzip support

--- go/remotohttp/encode.go
+++ go/remotohttp/encode.go
@@ -1,21 +1,31 @@
 package remotohttp
 
 import (
+	"compress/gzip"
 	"encoding/json"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
 // Encode writes the response.
-func Encode(w http.ResponseWriter, _ *http.Request, status int, v interface{}) error {
+func Encode(w http.ResponseWriter, r *http.Request, status int, v interface{}) error {
 	b, err := json.Marshal(v)
 	if err != nil {
 		return errors.Wrap(err, "encode json")
 	}
+	var out io.Writer = w
+	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		w.Header().Set("Content-Encoding", "gzip")
+		gzw := gzip.NewWriter(w)
+		out = gzw
+		defer gzw.Close()
+	}
 	w.Header().Set("Content-Type", "application/json; chatset=utf-8")
 	w.WriteHeader(status)
-	if _, err := w.Write(b); err != nil {
+	if _, err := out.Write(b); err != nil {
 		return err
 	}
 	return nil
